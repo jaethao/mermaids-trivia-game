b90679456ae88c72d53cc4593c99d1863a01eb5f
jaethao <mjathao@gmail.com>
2017-12-19 18:42:23 -0600

    slideshow, next and restart button

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,1 +1,30 @@
+//Global varibles
+//counter
+var correct = 0;
+var incorrect = 0;
 
+$(document).ready(function(){
+
+//slideshow
+  var slideIndex = 1;
+showDivs(slideIndex);
+
+function plusDivs(n) {
+  showDivs(slideIndex += n);
+}
+
+function showDivs(n) {
+  var i;
+  var x = document.getElementsByClassName("mySlides");
+  if (n > x.length) {slideIndex = 1}
+  if (n < 1) {slideIndex = x.length}
+  for (i = 0; i < x.length; i++) {
+     x[i].style.display = "none";
+  }
+  x[slideIndex-1].style.display = "block";
+}
+
+
+
+
+});
